10d75b6886b6dffa06a7ff44a638db69b697ac2e
Era Liwoch <era.liwoch@gmail.com>
2024-08-25 22:22:23 +0200

    Render tasks list from state array and add initial Task interface

--- src/features/SidebarTasksList.tsx
+++ src/features/SidebarTasksList.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import { Task } from "../interfaces";
 
 export function SidebarTasksList() {
+  const testData:Task[] = [
+    {
+      id: 1,
+      name: "first task"
+    },
+    {
+      id: 2,
+      name: "second task"
+    }
+  ];
+
+  const [ tasks, setTasks ] = useState<Task[]>(testData);
+
   return (
+    <>
     <aside>
-      SidebarTasksList cmp
-    </aside>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
+    </aside></>
   )
 }
