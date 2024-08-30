8e6d8d5a7cabd618959576db2327431f3f4f8227
Era Liwoch <era.liwoch@gmail.com>
2024-08-30 14:00:19 +0200

    Initially setup DnD (needs improvement - droppable not working)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { DndContext } from '@dnd-kit/core';
 import './App.css'
 import { LoginForm } from './components/LoginForm'
 import { Quadrant } from './components/Quadrant';
@@ -8,11 +9,13 @@ function App() {
   return (
     <div className='grid grid-cols-5 grid-rows-11 gap-4 inset-0 m-0 p-0'>
       <LoginForm />
-      <SidebarTasksList />
-      <Quadrant quadrant={"q1"} />
-      <Quadrant quadrant={"q2"} />
-      <Quadrant quadrant={"q3"} />
-      <Quadrant quadrant={"q4"} />
+      <DndContext>
+        <SidebarTasksList />
+        <Quadrant quadrant={"q1"} />
+        <Quadrant quadrant={"q2"} />
+        <Quadrant quadrant={"q3"} />
+        <Quadrant quadrant={"q4"} />
+      </DndContext>
     </div>
   );
 }
--- src/components/TaskLabel.tsx
+++ src/components/TaskLabel.tsx
@@ -1,9 +1,18 @@
+import { useDraggable } from "@dnd-kit/core";
 import { Task } from "../types";
+import {CSS} from '@dnd-kit/utilities';
 
 export function TaskLabel({task}:{task:Task}) {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: task.id+"draggable",
+  });
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
+
   return (
-    <article className="rounded border-2 border-indigo-700 bg-indigo-400 text-white font-bold my-2 p-2 text-sm">
-        { task.name }
+    <article className="rounded border-2 border-indigo-700 bg-indigo-400 text-white font-bold my-2 p-2 text-sm" ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      { task.name }
     </article>
   )
 }
