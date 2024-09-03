7727a66e062d0e29d7cd5f4fa45a93eff12079c4
Era Liwoch <era.liwoch@gmail.com>
2024-09-03 02:34:39 +0200

    Cast quadrants array to be observed in AssistantContainer, add rxjs dependencies

--- src/components/Assistant/AssistantContainer.tsx
+++ src/components/Assistant/AssistantContainer.tsx
@@ -1,13 +1,18 @@
+import { assistant } from "../../services/assistant.service";
 import { EMCategory, Task } from "../../types";
 import { TaskLabel } from "../TaskLabel";
 
 export function AssistantContainer({quadrant = "q1", tasks = []}:{quadrant: EMCategory, tasks: Task[]}) {
-    let a = () => {
-        if(tasks.length > 0) return (<TaskLabel task={tasks[0]} />)
-    };
-    return (
-      <div className="fixed_center bg-white">
-        {a()}
-      </div>
-    )
+  assistant.getTasksQuadrants().subscribe(
+    (quadrants:EMCategory[]) => console.log(quadrants)
+  );
+
+  let a = () => {
+      if(tasks.length > 0) return (<TaskLabel task={tasks[0]} />)
+  };
+  return (
+    <div className="fixed_center bg-white">
+      {a()}
+    </div>
+  )
 }
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,5 +1,6 @@
-import { Task } from "../types";
+import { EMCategory, Task } from "../types";
 import { Button } from "./ui_elements/Button";
+import { assistant } from "../services/assistant.service";
 
 export interface ControlsProperties {
     q1: Task[],
@@ -9,20 +10,21 @@ export interface ControlsProperties {
 }
 
 export function Controls(props: ControlsProperties) {
-    function tmpLogTasks() {
-        console.log(props.q1);
-        console.log(props.q2);
-        console.log(props.q3);
-        console.log(props.q4);
-    }
+    function isDisabled():boolean {
+        if(props.q1.length < 4 && props.q2.length < 1 && props.q3.length < 1 && props.q4.length < 1) return true;
+        return false;
+    };
 
-    function isDisabled() {
-        if(props.q1.length < 1 && props.q2.length < 1 && props.q3.length < 1 && props.q4.length < 1) return true;
-    }
+    function evaluateMatrix():void {
+        const quadrantsToAssist: EMCategory[] = [];
+        if(props.q1.length > 3) 
+            quadrantsToAssist.push("q1");
+        assistant.setTasksQuadrants(quadrantsToAssist);
+    };
 
     return (
         <footer className="col-span-5 row-span-1">
-            <Button text="Apply" fn={tmpLogTasks} disabled={isDisabled()} />
+            <Button text="Apply" fn={evaluateMatrix} disabled={isDisabled()} />
         </footer>
     );
 }
