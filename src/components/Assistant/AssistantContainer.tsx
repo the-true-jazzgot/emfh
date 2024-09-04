0ad9a8ae16aa757e9fcce61d186f12aa428775ac
Era Liwoch <era.liwoch@gmail.com>
2024-09-04 04:04:03 +0200

    Render quadrant assistant for first element in array

--- src/components/Assistant/AssistantContainer.tsx
+++ src/components/Assistant/AssistantContainer.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import { assistant } from "../../services/assistant.service";
 import { EMCategory, Task } from "../../types";
+import { Q1Assistant } from "./Q1Assistant";
 
 export function AssistantContainer() {
-  assistant.getTasksQuadrants().subscribe(
-    (quadrants:EMCategory[]) => console.log(quadrants)
-  );
+  const [quadrants, setQuadrants] = useState<EMCategory[]>([]);
+
+  const renderFirstQuadrantAssistant = () => { 
+    if(quadrants.length > 0) {
+      switch(quadrants[0]) {
+        case "q1":
+          return <Q1Assistant />;
+          break;
+        default:
+          return "";
+      }
+    }
+  }
+
+  useEffect(() => {
+    const subscription = assistant.getTasksQuadrants().subscribe(
+      (receivedQuadrants: EMCategory[]) => {
+        setQuadrants(receivedQuadrants);
+      }
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [quadrants]); 
 
   return (
     <div className="fixed_center bg-white">
+      {renderFirstQuadrantAssistant()}
     </div>
   )
 }
---
+++ src/components/Assistant/Q1Assistant.tsx
@@ -0,0 +1,5 @@
+export function Q1Assistant() {
+  return (
+    <h4>Assistant for Q1</h4>
+  )
+}
