e3865bfd397aaa34bff8086d08e57fe89d4ec185
Era Liwoch <era.liwoch@gmail.com>
2024-09-05 05:27:07 +0200

    Apply some overcomplicated rxjs logic that doesn't work

--- src/components/Quadrant.tsx
+++ src/components/Quadrant.tsx
@@ -3,10 +3,26 @@ import { EMCategory, Task } from "../types";
 import { TaskLabel } from "./TaskLabel";
 import { useEffect, useState } from "react";
 import { filterData, toDosQuery } from "../services/tasks.service";
+import { TasksActionsFactory, TasksDispatchFactory, TasksListAction } from "../services/dnd.service";
 
 export function Quadrant({quadrant}:{quadrant: EMCategory}) {
   const { data, isSuccess } = toDosQuery()
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [ tasks, setTasks ] = useState<Task[]>([]);
+
+  function moveTask(taskId: string, moveTo: EMCategory):void {
+    console.log("Move task: "+taskId+"from: "+quadrant+" to: "+moveTo);
+    const task = tasks.find(item => item.id === taskId);
+    if(!!task) {
+      setTasks(
+        tasks.filter(item => item.id !== taskId)
+      );
+      TasksDispatchFactory[moveTo]({taskId: taskId, task: task, action: "add"} as TasksListAction);
+    }
+  }
+
+  function addTask(task: Task):void {
+    setTasks([...tasks, task]);
+  }
 
   useEffect(()=>{
     isSuccess?
@@ -17,6 +33,20 @@ export function Quadrant({quadrant}:{quadrant: EMCategory}) {
   const {setNodeRef} = useDroppable({
     id: quadrant
   });
+
+  useEffect(() => {
+    const subscription = TasksActionsFactory[quadrant]().subscribe(
+      (action: TasksListAction) => {
+        console.log(action);
+        if(action.action ===  "move") moveTask(action.taskId, action.moveTo || "uncategorized");
+        if(action.action === "add" && !!action.task) addTask(action.task);
+      }
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [tasks]); 
 
   return (
     <section id={quadrant} ref={setNodeRef} className={"js-" + quadrant + " col-span-2 row-span-5 rounded bg-container p-2"} >
--- src/services/dnd.service.ts
+++ src/services/dnd.service.ts
@@ -1,24 +1,94 @@
 import { DragEndEvent } from "@dnd-kit/core";
-import { EMCategory } from "../types";
+import { EMCategory, Task } from "../types";
+import { Observable, Subject } from 'rxjs';
 
-  export const addTaskToComponent = (e: DragEndEvent) => {
-    const taskId: string = e.active.id.toString();
-    const task: HTMLElement | null = document.getElementById(taskId);
+export interface TasksListAction {
+  taskId: string,
+  action: "add" | "move",
+  moveTo?: EMCategory,
+  task?: Task
+}
 
-    if(e.over?.id && !!task) {
-      const currCategory:EMCategory = task.dataset.category as EMCategory || "uncategorized";
-      const newCategory: EMCategory = e.over?.id as EMCategory;
+const uncategorizedSubject:Subject<TasksListAction> = new Subject();
+const uncategorizedDispatch = (task: TasksListAction):void => uncategorizedSubject.next(task);
+const uncategorizedAction = ():Observable<TasksListAction> => uncategorizedSubject.asObservable();
 
-      console.log(currCategory, newCategory);
-    //   const currCatState = categoryStates[currCategory];
-    //   const newCatState = categoryStates[newCategory];
+const q1Subject:Subject<TasksListAction> = new Subject();
+const q1Dispatch = (task: TasksListAction):void => q1Subject.next(task);
+const q1Action = ():Observable<TasksListAction> => q1Subject.asObservable();
 
-    //   const taskToMove: Task | undefined = currCatState.readState.find((item: { id: string; }) => item.id === taskId);
-    //   if(!!taskToMove) {
-    //     currCatState.setState(currCatState.readState.filter((item: Task) => item !== taskToMove));
-    //     console.log(newCatState.id);
-    //     taskToMove.category = newCatState.id;
-    //     newCatState.setState([...newCatState.readState,taskToMove]);
-    //   }
+const q2Subject:Subject<TasksListAction> = new Subject();
+const q2Dispatch = (task: TasksListAction):void => q2Subject.next(task);
+const q2Action = ():Observable<TasksListAction> => q2Subject.asObservable();
+
+const q3Subject:Subject<TasksListAction> = new Subject();
+const q3Dispatch = (task: TasksListAction):void => q3Subject.next(task);
+const q3Action = ():Observable<TasksListAction> => q3Subject.asObservable();
+
+const q4Subject:Subject<TasksListAction> = new Subject();
+const q4Dispatch = (task: TasksListAction):void => q4Subject.next(task);
+const q4Action = ():Observable<TasksListAction> => q4Subject.asObservable();
+
+export const addTaskToComponent = (e: DragEndEvent) => {
+  const taskId: string = e.active.id.toString();
+  const task: HTMLElement | null = document.getElementById(taskId);
+
+  if(e.over?.id && !!task) {
+    const currCategory:EMCategory = task.dataset.category as EMCategory || "uncategorized";
+    const newCategory: EMCategory = e.over?.id as EMCategory
+    const move: TasksListAction = { taskId: taskId, action: "move", moveTo: newCategory };
+    
+    switch(currCategory) {
+      case "uncategorized":
+        console.log("Move task: "+taskId+" from: "+currCategory+" to: "+ newCategory);
+        uncategorizedDispatch(move);
+        break;
+      case "q1":
+        q1Dispatch(move);
+        break;
+      case "q2":
+        q2Dispatch(move);
+        break;
+      case "q3":
+        q3Dispatch(move);
+        break;
+      case "q4":
+        q4Dispatch(move);
+        break;
     }
-  };
+
+    // switch(newCategory) {
+    //   case "uncategorized":
+    //     uncategorizedDispatch(add);
+    //     break;
+    //   case "q1":
+    //     q1Dispatch(add);
+    //     break;
+    //   case "q2":
+    //     q2Dispatch(add);
+    //     break;
+    //   case "q3":
+    //     q3Dispatch(add);
+    //     break;
+    //   case "q4":
+    //     q4Dispatch(add);
+    //     break;
+    // }
+  }
+};
+
+export const TasksActionsFactory: Record<EMCategory, () => Observable<TasksListAction>> = {
+  "uncategorized": uncategorizedAction,
+  "q1": q1Action,
+  "q2": q2Action,
+  "q3": q3Action,
+  "q4": ():Observable<TasksListAction> => q4Subject.asObservable()
+}
+
+export const TasksDispatchFactory: Record<EMCategory, (task: TasksListAction) => void> = {
+  "uncategorized": uncategorizedDispatch,
+  "q1": q1Dispatch,
+  "q2": q2Dispatch,
+  "q3": q3Dispatch,
+  "q4": q4Dispatch
+}
